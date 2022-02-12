02344b938efe6ea11e487ef36b0e9065ac23daca
Rushikesh Rajendra Kale <75717564+Rushi4001@users.noreply.github.com>
2022-02-12 12:23:14 +0530

    Add files via upload

---
+++ practice_LWC_Project/force-app/main/default/lwc/button/button.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+export default class Button extends LightningElement {
+
+    clickedButtonLabel;
+
+    handleClick(event) {
+        this.clickedButtonLabel = event.target.label;
+    }
+}
